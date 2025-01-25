52f0d458fff4551dcfd93369a3f23505427b3fa3
Arturo <rainthstrive@gmail.com>
2025-01-25 17:40:18 -0600

    Consume Items

--- main.lua
+++ main.lua
@@ -3,17 +3,34 @@ local p1
 function love.load()
     Object = require "classic"
     local Player = require "player"
-    p1 = Player(400, 300, 100, 100)
+    local Food = require "food"
+    p1 = Player(400, 300, 30, 30)
+    foodList = {}
+    for i=1, 5 do
+        table.insert(
+            foodList,
+            Food(math.random(50, 650), math.random(50, 450), 25)            
+        )
+    end
 end
 
 function love.update(dt)
     p1:update(dt)
+    for i,v in ipairs(foodList) do
+        if checkCollision(p1, v) then
+            table.remove(foodList, i)
+        end
+    end
 end
 
 function love.draw()
     p1:draw()
+    for i,v in ipairs(foodList) do
+        v:draw()
+    end
 end
 
-function loadPlayer()
-    
+function checkCollision(object, colObject) 
+    local distance = math.sqrt((object.x - colObject.x)^2 + (object.y - colObject.y)^2)
+    return distance < object.radius + colObject.radius
 end
--- player.lua
+++ player.lua
@@ -2,7 +2,7 @@ local Shape = require "shape"
 local Player = Shape:extend()
 
 function Player:Rotate(direction_txt, dt)
-    local rotationSpeed_float = math.rad(90)
+    local rotationSpeed_float = math.rad(180)
     if direction_txt=="right" then
         self.rotationAngle_float = self.rotationAngle_float + rotationSpeed_float * dt
     elseif direction_txt=="left" then
@@ -21,10 +21,9 @@ function Player:Move(dt)
     self.y = self.y + dy * self.speed * dt
 end
 
-function Player:new(x, y, width, height)
+function Player:new(x, y, radius)
     Player.super.new(self, x, y)
-    self.width = width
-    self.height = height
+    self.radius = radius
     self.rotationAngle_float = 0
 end
 
@@ -42,7 +41,7 @@ function Player:draw()
     love.graphics.push()
         love.graphics.translate(self.x, self.y)
         love.graphics.rotate(self.rotationAngle_float)
-        love.graphics.rectangle("line", -self.width/2, -self.height/2, self.width, self.height)
+        love.graphics.circle("line", 0, 0, self.radius)
     love.graphics.pop()
 end
 
